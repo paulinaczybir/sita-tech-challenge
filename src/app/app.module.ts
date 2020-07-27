fa00de4c8f3a7f974875c0e15598f3089c761e5d
Paulina Czybir <paulina.czybir@gmail.com>
2020-07-27 16:37:52 +0100

    Added basic components structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { CommentsTableComponent } from './components/comments-table/comments-table.component';
+import { FlightComponent } from './components/flight/flight.component';
+import { NewCommentFormComponent } from './components/new-comment-form/new-comment-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidenavComponent,
+    CommentsTableComponent,
+    FlightComponent,
+    NewCommentFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/comments-table/comments-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comments-table',
+  templateUrl: './comments-table.component.html',
+  styleUrls: ['./comments-table.component.css']
+})
+export class CommentsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
