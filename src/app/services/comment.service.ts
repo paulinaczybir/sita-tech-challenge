99c8bc8fb19d328b4996af457c52fcb91dd209c5
Paulina Czybir <paulina.czybir@gmail.com>
2020-07-28 18:13:18 +0100

    Post comment works.

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Comment } from '../models/Comment';
+
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private http: HttpClient) { }
+
+  addComment(comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>('/api/', comment);
+  }
+}
